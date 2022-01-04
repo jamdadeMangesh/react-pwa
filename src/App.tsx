dae87fe686ed15211d735f0f09a9693eb8322fd4
Mangesh Jamdade <jamdademangesh1993.com>
2022-01-04 11:49:11 +0530

    Added description about PWA

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,74 @@
 import React from 'react';
 import './App.css';
 import pwaIcon from "./images/pwa.svg";
+import indexImg from "./images/index.png";
+import manifestImg from "./images/manifest.png";
 
 function App() {
 	return (
 		<div className="App-header">
 			<header className="container">
-				<h3>React Progressive Web App</h3>
-				<img src={pwaIcon} alt="pwa-logo" />
+				<div className="col-md-6 offset-md-3">
+					<h3 className="text-center">React Progressive Web App</h3>
+					<img src={pwaIcon} alt="pwa-logo" className="mx-auto d-block" />
 
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-				<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-				<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+					<h5 className="mt-4">Steps to create Progressive Web App</h5>
+					<hr />
+					<div className="pwa-info">
+						<h6>1. Create new react app with PWA</h6>
 
+						<div>Without Typescript</div>
+						<code className="pwa-code mt-2">
+							<span>npx create-react-app my-app --template cra-template-pwa</span>
+						</code>
+
+						<div className="mt-2">With Typescript</div>
+						<code className="pwa-code mt-2">
+							<span>npx create-react-app my-app --template cra-template-pwa-typescript</span>
+						</code>
+					</div>
+
+					<div className="pwa-info mt-4">
+						<h6>2. Make change in index.tsx</h6>
+						<div>Inside src folder, on line no.18 change <strong>serviceWorkerRegistration.unregister();</strong> to <strong>serviceWorkerRegistration.register();</strong></div>
+						<img src={indexImg} alt="index file" className="mx-auto d-block img-fluid mt-2" />
+					</div>
+
+					<div className="pwa-info mt-4">
+						<h6>2. Update manifest.json</h6>
+						<div>Inside public folder, update manifest.json.</div>
+						<img src={manifestImg} alt="index file" className="mx-auto d-block img-fluid mt-2" />
+
+						<ul className="pwa-info mt-4">
+							<li><strong>short_name:</strong> The name of your application, used within the icon, like on the users’ home screens or launchers.</li>
+							<li><strong>name:</strong> The name of your app shown in the app stores or browsers on startup screens and prompts. If name property is not defined, short_name will be displayed.</li>
+							<li><strong>icons:</strong> The set of icons (or just one), used on the users’ home screens, launchers, task switchers, splash screens, etc.</li>
+							<li><strong>start_url:</strong> The URL of the page of your app that your users see on the startup.</li>
+							<li><strong>display:</strong> This property is responsible for the browser view. The app can hide the address bar, run in the new window, go fullscreen, etc. See the attributes you can use for it:
+								<ul>
+									<li><strong>fullscreen:</strong> This option is for opening the app without any browser UI, occupying the entirety of the users’ display.</li>
+									<li><strong>standalone:</strong> This option allows for running the app in the new window, like a native app. It will also hide the browser UI elements like an address bar.</li>
+									<li><strong>minimal-ui:</strong> This option is quite similar to the previous one, but it comes with the minimal set of browser UI, like back and reload buttons.</li>
+									<li><strong>browser:</strong> This option enables the usual browser experience.</li>
+								</ul>
+							</li>
+							<li><strong>theme_color:</strong> The color of the toolbar in your app.</li>
+							<li><strong>background_color:</strong> The color of the splash screen that appears when users click on the app icon in their home screens.​</li>
+
+						</ul>
+					</div> 
+
+					<div className="pwa-info mt-4">
+						<h6>4. Add manifest.json inside index.html</h6>
+						<div>To make your web manifest file work, you need to add it to the pages of your web app, i.e inside <strong>public/index.html.</strong></div>
+					</div>
+
+					<div className="pwa-info mt-4">
+						<h6>5. Test your PWA</h6>
+						<div>To test PWA, we need to make it live on <strong>HTTPS Server.</strong></div>
+					</div>
+					
+				</div>
 			</header>
 		</div>
 	);
